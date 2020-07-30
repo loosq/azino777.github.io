34e624fb51d9dcf49431a9bd9af5f2a73e6c9cd6
Полонский Сергей <Polonskiy.S.A@sberbank.ru>
2020-07-30 10:19:35 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,68 @@
+var wrapper = $('.body-wrapper');
+
+// var submitForm = function () {
+//     wrapper.on('submit', 'form', function (e) {
+//         e.preventDefault();
+//         console.log($(this).serializeArray());
+//     })
+// }
+
+var ifFormReady = function () {
+    let value = 0;
+    wrapper.on('change', '.make-order__form input[type="checkbox"]', function (e) {
+        const $this = $(this),
+            form = $this.closest('form'),
+            item = $this.closest('.make-order__list-item'),
+            btn = $('button', form),
+            textInput = $('.make-order__list-item--text-input', item);
+
+        //change button
+        //console.log($this);
+        if ($('input:checked', form).length) {
+            btn.removeClass("disabled");
+        } else {
+            btn.addClass("disabled");
+        }
+
+        //change total summ
+        if ($this.prop('checked')) {
+            $('input', textInput).val('');
+            textInput.fadeIn(300);
+            value += parseInt($this.val());
+        } else {
+            textInput.fadeOut(300);
+            value -= parseInt($this.val());
+        }
+
+        $('.make-order__list-total--value span', form).text(value);
+    })
+}
+
+var ifContactFormReady = function () {
+    var data = {
+            name: '',
+            phone: '',
+            mail: ''
+        },
+        form = $('.complete-order-form form');
+
+    wrapper.on('keyup', '.complete-order-form-inner input[type="text"]', function (e) {
+        const $this = $(this);
+        data[$this.prop('name')] = $this.val();
+    });
+
+    form.on('submit', function (e) {
+        e.preventDefault();
+        $(this).serializeArray().forEach(function (item) {
+            if ($('input', form).prop('name')) {
+
+            }
+        })
+    })
+}
+
+$(document).ready(function () {
+    // submitForm();
+    ifFormReady();
+    ifContactFormReady();
+});
